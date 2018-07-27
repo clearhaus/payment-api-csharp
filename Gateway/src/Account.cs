50e2f5a10862d6d90b5c003c5ac11269671c10d6
Kasper Sacharias Eenberg <kse@clearhaus.com>
2018-07-27 15:26:04 +0200

    Refactor AccountInfo

--- Gateway/src/Account.cs
+++ Gateway/src/Account.cs
@@ -190,21 +190,6 @@ namespace Clearhaus.Gateway
         public string csc;
     }
 
-    public class AccountInfo
-    {
-        [JsonProperty("merchant_id")]
-        public string merchantID { get; set; }
-        public string descriptor { get; set; }
-        public string name { get; set; }
-        public string country { get; set; }
-        public int mcc { get; set; }
-
-        public Acquirer acquirer {get; set;}
-
-        [JsonProperty("transaction_rules")]
-        public string transactionRules { get; set; }
-    }
-
     public class Acquirer
     {
         [JsonProperty("visa_bin")]
