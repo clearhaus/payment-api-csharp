addc1f6f7ef9761773879e6456217a833b0f4cdf
Kasper Sacharias Eenberg <kse@clearhaus.com>
2018-08-23 21:15:23 +0200

    Refactor exceptions into own file

--- Util/src/RestRequest.cs
+++ Util/src/RestRequest.cs
@@ -177,32 +177,3 @@ namespace Clearhaus.Util
         }
     }
 }
-
-namespace Clearhaus
-{
-    public class ClrhsException : Exception
-    {
-        public ClrhsException() : base() {}
-        public ClrhsException(string msg) : base(msg) {}
-        public ClrhsException(string msg, Exception exc) : base(msg, exc) {}
-    }
-
-    public class ClrhsAuthException : ClrhsException
-    {
-        public ClrhsAuthException() : base() {}
-        public ClrhsAuthException(string msg) : base(msg) {}
-    }
-
-    public class ClrhsGatewayException : ClrhsException
-    {
-        public ClrhsGatewayException() : base() {}
-        public ClrhsGatewayException(string msg) : base(msg) {}
-    }
-
-    public class ClrhsNetException : ClrhsException
-    {
-        public ClrhsNetException() : base() {}
-        public ClrhsNetException(string msg) : base(msg) {}
-        public ClrhsNetException(string msg, Exception exc) : base(msg, exc) {}
-    }
-}
