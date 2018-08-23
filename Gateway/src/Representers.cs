e37489c16f7de87f90f5751330f30dc1e24a6f68
Kasper Sacharias Eenberg <kse@clearhaus.com>
2018-08-23 21:01:28 +0200

    Allow omitting csc

--- Gateway/src/Representers.cs
+++ Gateway/src/Representers.cs
@@ -63,6 +63,19 @@ namespace Clearhaus.Gateway
         /// <param name="pan">Primary Account Number"</param>
         /// <param name="expireMonth">Month of card expiry</param>
         /// <param name="expireYear">Year of card expiry</param>
+        public Card(string pan, string expireMonth, string expireYear)
+        {
+            this.pan         = pan;
+            this.expireMonth = expireMonth;
+            this.expireYear  = expireYear;
+        }
+
+        /// <summary>
+        /// Construct a new card
+        /// </summary>
+        /// <param name="pan">Primary Account Number"</param>
+        /// <param name="expireMonth">Month of card expiry</param>
+        /// <param name="expireYear">Year of card expiry</param>
         /// <param name="csc">CSC</param>
         public Card(string pan, string expireMonth, string expireYear, string csc)
         {
